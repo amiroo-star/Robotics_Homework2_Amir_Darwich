44a20b03889db6abe33a875dbe2933e2abaad8fc
Amir <afd11@mail.aub.edu>
2026-07-04 14:00:17 +0000

    feat: add Robot abstract base class

---
+++ include/robot.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include<string>
+#include<ostream>
+class Task;
+class Robot{
+public:
+	Robot(const std::string& id,const std::string& name,int battery);
+	virtual~Robot()=default;
+	virtual void work=()=0;
+	virtual void charge();
+	virtual std::string type() const=0;
+	std::string id() const;
+std::strong name() const;
+int battery() const;
+std::string status() const;
+bool operatoor== (const Robot& other) const;
+std::string operator+(const Robot& other) const;
+friend std::ostream& operator<< (std::ostream& os,const Robot&r);
+protected:
+std::string id_:
+std::string name_;
+int battery_;
+std::string status_;};

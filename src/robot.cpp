4a48a039c25c7d8269b1df73a180134c6af7a6ae
Amir <afd11@mail.aub.edu>
2026-07-04 18:10:44 +0000

    feat: Robot Class

---
+++ src/robot.cpp
@@ -0,0 +1,18 @@
+#include "robot.hpp"
+#include<algorithm>
+#include<stdexcept>
+//instead of rebuilding everything we only call the class
+Robot::Robot(const std::string& id, const std::string&name, int battery):id_(id),name_(name),battery_(std::clamp(battery,0,100)),status_("idle"){}
+void Robot:: charge(){battery_=100;
+status_="charging";
+}
+std::string::id() const{return id_;}
+std::string Robot::name() const{return name_;}
+int Robot::battery() const{return battery_;}
+std::string Robot::status() const{return status_;}
+bool Robot::operator==(const Robot& other)const{return id_==other.od_;}
+std::string Robot::operator+(const Robot& other) const{return name_+" + "+other.name_;}
+std::ostream& operator<<(std::ostream& os, const Robot& r){ os<<"["<<r.type()<<"]"<<r.name_<<"(id:"<<r.id_<<")"<<"battery: "<<r.battery_<<"%"<<"status:"<<r.status_;
+return os;}
+
+
